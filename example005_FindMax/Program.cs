73996e72726cf2a4e4b8043c7afba20c55f08c36
Valentin Gavrilov <valentin-s-gavrilov@yandex.ru>
2022-12-13 22:40:46 +0300

    FindMax

---
+++ example005_FindMax/Program.cs
@@ -0,0 +1,29 @@
+﻿int a = 4;
+int b = 71;
+int c = 1;
+int d = 9;
+int e = 3;
+int max = a;
+
+if (a > max)
+{
+    max = a;
+}
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
