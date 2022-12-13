dd7b2d24081b6c63a88a8ef8ed76d481f48d8ea2
Valentin Gavrilov <valentin-s-gavrilov@yandex.ru>
2022-12-13 22:25:44 +0300

    HelloSpecUser

---
+++ example003_HelloSpecUser/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter yor name");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "маша")
+{
+    Console.WriteLine("Урааа! Это же Маша пришла!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
